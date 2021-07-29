4742c3711afe599cafb63c31340975f5b18e78c0
soumya12392 <83289414+soumya12392@users.noreply.github.com>
2021-07-29 12:38:44 +0530

    Add files via upload
    Completed word game

--- game_page.js
+++ game_page.js
@@ -41,3 +41,49 @@ function send() {
     document.getElementById("output").innerHTML = row;
     document.getElementById("word").value = "";
 }
+
+var get_word = document.getElementById("word").value;
+var word = get_word.toLowerCase();
+
+var question_turn = "player_1";
+var answer_turn = "player_2";
+
+function check() {
+    var get_answer = document.getElementById("input_check_box").value;
+    var answer = get_answer.toLowerCase();
+    console.log("Answer in lowercase = " + answer);
+
+    if (answer == word) {
+        if(answer_turn == "player_1") {
+            player1_score += 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else {
+            player2_score += 1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+
+    console.log("Player 1 = " + player1_score);
+    console.log("Player 2 = " + player2_score);
+
+    if (question_turn == "player_1") {
+        question_turn = "player_2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else {
+        question_turn = "player_1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+
+    if (answer_turn == "player_1") {
+        answer_turn = "player_2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+    else {
+        answer_turn = "player_1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
+}
